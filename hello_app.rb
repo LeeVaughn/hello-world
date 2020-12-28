70d766e3ce0f9681bc62c42f9bd81236761ed7be
LeeVaughn <dlvprogramming@gmail.com>
2020-12-28 13:41:46 -0800

    Update method to use Date class

--- hello_app.rb
+++ hello_app.rb
@@ -1,8 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"]
-  dayname = DAYNAMES[Time.now.wday]
+  dayname = Date::DAYNAMES[Time.now.wday]
 
   "Hello, world! Happy #{dayname}."
 end
